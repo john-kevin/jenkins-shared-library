6ac358fdec0c11e16ec714f041523c34874d3595
JohnKevinSoriano <john.soriano@iselect.com.au>
2020-09-14 09:23:08 +0800

    rwar

--- vars/logs.groovy
+++ vars/logs.groovy
@@ -4,3 +4,25 @@ def info(message) {
     println "The message below was invoked in this function"
     echo "${message}"
 }
+
+
+def checkout(String branch) {
+    echo "Checking out branch ${branch}"
+    checkout([
+        $class: 'GitSCM',
+        branches: [
+            [
+                name: "${branch}"
+            ]
+        ],
+        doGenerateSubmoduleConfigurations: false,
+        extensions: [],
+        submoduleCfg: [],
+        userRemoteConfigs: [
+            [
+                credentialsId: 'private-repo',
+                url: env.GIT_URL
+            ]
+        ]
+    ])
+}
