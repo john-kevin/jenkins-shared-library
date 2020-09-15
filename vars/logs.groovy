00d36d22788af9edea07ed6401d642fc872a3cd5
john-kevin <kevinsoriano2828@gmail.com>
2020-09-15 14:09:35 +0800

    t

--- vars/logs.groovy
+++ vars/logs.groovy
@@ -35,7 +35,7 @@ def printer(String message) {
         echo "kuha mo!"
     }
     // String[][] arrStr = [message, 'awsapi', 'awsstg', 'version']
-    def colors = [red: "#FF0000", green:"#00FF00", blue: "#0000FF", kev: MENSAHE]
+    def colors = [red: "#FF0000", green:"#00FF00", blue: "#0000FF"]
     // def doctor = [name: "Oliver","block-no":33,speciality:"Cardiology"]
     return colors
 }
